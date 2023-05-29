7d49a7a788198a9c1e4f139ac636e659eb78fd29
Boris <89241300089@mail.ru>
2023-05-29 09:53:57 +0900

    Added Task3

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -7,25 +7,32 @@ A (7,-5, 0); B (1,-1,9) -> 11.53
 class Program {
     public static void Main (string[] args) {
         Console.Write("Введите координату X первой точки: ");
-        int x1 = 0;
-        x1 = int.Parse(Console.ReadLine());
+        int x1 = GetNmber();//int.Parse(Console.ReadLine());
         Console.Write("Введите координату Y первой точки: ");
-        int y1 = 0;
-        y1 = int.Parse(Console.ReadLine());
+        int y1 = GetNmber();//int.Parse(Console.ReadLine());
         Console.Write("Введите координату Z первой точки: ");
-        int z1 = 0;
-        z1 = int.Parse(Console.ReadLine());
+        int z1 = GetNmber();//int.Parse(Console.ReadLine());
         Console.Write("Введите координату X второй точки: ");
-        int x2 = 0;
-        x2 = int.Parse(Console.ReadLine());
+        int x2 = GetNmber();//int.Parse(Console.ReadLine());
         Console.Write("Введите координату Y второй точки: ");
-        int y2 = 0;
-        y2 = int.Parse(Console.ReadLine());
+        int y2 = GetNmber();//int.Parse(Console.ReadLine());
         Console.Write("Введите координату Z второй точки: ");
-        int z2 = 0;
-        z2 = int.Parse(Console.ReadLine());
+        int z2 = GetNmber();//int.Parse(Console.ReadLine());
 
         double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
         Console.WriteLine($"Расстояние между точками X({x1}, {y1}, {z1}) и Y({x2}, {y2}, {z2}) равно {Math.Round(distance, 2)}");
+     }
+
+     static int GetNmber() {
+        String stringNumber = Console.ReadLine() ?? "Значение Null";
+        int number;
+        bool isNumber = int.TryParse(stringNumber, out number);
+        if (isNumber) {
+            return number;
+        }
+        else
+        {
+            return 0;
+        }
      }
 }
---
+++ Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+class Program {
+    public static void Main (string[] args) {
+      Console.Write("Введите число N: ");
+      int number = 0;
+      number = GetNmber();//int.Parse(Console.ReadLine());
+      for (int i = 1; i <= number; i++)
+        {
+            Console.Write(i == number ? Math.Pow(i, 3) : Math.Pow(i, 3) + ", ");
+        }
+      Console.WriteLine();
+    }
+
+    static int GetNmber() {
+        String stringNumber = Console.ReadLine() ?? "Значение Null";
+        int number;
+        bool isNumber = int.TryParse(stringNumber, out number);
+        if (isNumber) {
+            return number;
+        }
+        else
+        {
+            return 0;
+        }
+     }
+}
